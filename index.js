e9a69efa137f8ff57f47e5c3b59bb12355877e97
Azim Gadzhiagayev <me@azimgd.com>
2021-01-14 15:46:29 +0500

    Remove react native project;

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// /**
-//  * @format
-//  */
-
-// import {AppRegistry} from 'react-native';
-// import App from './App';
-// import {name as appName} from './app.json';
-
-// AppRegistry.registerComponent(appName, () => App);
-
 module.exports = {
   Helpers: require('./dist/Helpers.js'),
   Queue: require('./dist/Queue.js'),
